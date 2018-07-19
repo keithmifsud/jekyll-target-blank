d627d77ccc55db439fc9074bc14c86a6f543125a
Keith Mifsud <mifsud.k@gmail.com>
2018-07-19 19:47:51 +0545

    GH-4 It can add multuple CSS classes.

--- spec/jekyll-target_spec.rb
+++ spec/jekyll-target_spec.rb
@@ -186,12 +186,13 @@ RSpec.describe(Jekyll::TargetBlank) do
   end
 
   context "Adds more than one CSS classes to the links" do
-    let(:target_blank_add_css_classes) { ["some-class", "other-some-class", "another-some-class" ] }
     let(:config_overrides) do
-      { "target-blank" => { "add_css_class" => target_blank_add_css_classes } }
+      { "target-blank" => { "add_css_class" =>  "some-class other-some-class another-some-class" }}
     end
 
-
+    it "should add the CSS classes specified in config" do
+      expect(post_with_external_markdown_link.output).to include(para('Link to <a href="https://google.com" target="_blank" rel="noopener noreferrer" class="some-class other-some-class another-some-class">Google</a>.'))
+    end
   end
 
   private
