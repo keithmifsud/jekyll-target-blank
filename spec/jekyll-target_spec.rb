6f8f17fbf38f7ba98691c1e9109fb32c9c51f272
Keith Mifsud <mifsud.k@gmail.com>
2018-07-23 17:33:23 +0545

    GH-4 Fixed extra line break in spec file.

--- spec/jekyll-target_spec.rb
+++ spec/jekyll-target_spec.rb
@@ -187,14 +187,13 @@ RSpec.describe(Jekyll::TargetBlank) do
     it "should add the CSS class specified in config even when the link already has a CSS class specified" do
       expect(post_with_html_link_containing_the_specified_css_class.output).to include(para('<a href="https://google.com" class="some-class ext-link" target="_blank" rel="noopener noreferrer">Link with the css class specified in config</a>.'))
     end
-    
+
     it "should add the CSS class specified in config even when the link has more than CSS classes already included" do
       expect(post_with_external_link_containing_the_specified_css_class_and_other_css_classes.output).to include(para('This is <a href="https://not-keith-mifsud.me" class="some-class random-class ext-link another-random-class" target="_blank" rel="noopener noreferrer">a link containing the specified css class and two other random css classes</a>.'))
     end
   end
 
   context "When more than one CSS classes are specified in config" do
-
     it "should add the CSS classes specified in config" do
       expect(post_with_external_markdown_link.output).to include(para('Link to <a href="https://google.com" target="_blank" rel="noopener noreferrer" class="some-class other-some-class another-some-class">Google</a>.'))
     end
