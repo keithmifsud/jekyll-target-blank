dff618a818c297fa14099ac22ebe5336075baf1e
Keith Mifsud <mifsud.k@gmail.com>
2018-07-27 15:03:58 +0545

    GH-3 Added a missing traling comma in Spec context configuration.

--- spec/jekyll-target_spec.rb
+++ spec/jekyll-target_spec.rb
@@ -300,7 +300,7 @@ RSpec.describe(Jekyll::TargetBlank) do
       {
         "target-blank" => {
           "add_css_classes" => false,
-          "rel" => rel_attribute
+          "rel" => rel_attribute,
         },
       }
     end
