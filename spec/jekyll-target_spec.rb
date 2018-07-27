637512fd001f581a1bab9b564277bacb18ffe6db
Keith Mifsud <mifsud.k@gmail.com>
2018-07-27 15:31:51 +0545

    GH-3 It can add more than one extra rel attribute values even when one default value should be excluded.

--- spec/jekyll-target_spec.rb
+++ spec/jekyll-target_spec.rb
@@ -340,6 +340,10 @@ RSpec.describe(Jekyll::TargetBlank) do
         },
       }
     end
+
+    it "should the extra rel attribute values and not add the default noopener value" do
+      expect(post_with_external_markdown_link.output).to include(para('Link to <a href="https://google.com" target="_blank" rel="noreferrer nofollow tag">Google</a>.'))
+    end
   end
   private
 
