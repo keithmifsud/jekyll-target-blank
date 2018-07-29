078c49fc83c5870731f31d64a4234cf1429a2769
Keith Mifsud <mifsud.k@gmail.com>
2018-07-29 09:07:07 +0545

    GH-18 Fixed missing space in main class.

--- lib/jekyll-target-blank.rb
+++ lib/jekyll-target-blank.rb
@@ -272,7 +272,7 @@ module Jekyll
       # Private: Gets any additional rel attribute values
       # values to add from config.
       def extra_rel_attribute_values_to_add
-        config =@target_blank_config
+        config = @target_blank_config
         config.fetch("rel")
       end
 
